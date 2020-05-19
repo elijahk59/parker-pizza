4a6bbbae03501bbc96f1727a6c16cd49441858e0
elijahk59 <elijahk59@gmail.com>
2020-05-19 23:55:52 +0300

    added business logic formula to calculate total price

--- js/script.js
+++ js/script.js
@@ -4,6 +4,57 @@ function Pizza(size, crust, toppings) {
     this.crust = crust;
     this.toppings = toppings;
 }
+var priceSize, priceCrust, priceTopping;
+
+//calculate pizza price
+var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
+    switch (pizzaSize) {
+        case "":
+            priceSize = 0;
+            break;
+        case "large":
+            priceSize = 1200;
+            break;
+        case "medium":
+            priceSize = 800;
+            break;
+        case "small":
+            priceSize = 400;
+            break;
+        default:
+            location.reload();
+            alert("Please select a pizza size");
+    };
+
+    switch (pizzaCrust) {
+        case "":
+            priceCrust = 0;
+            break;
+        case "crispy":
+            priceCrust = 100;
+            break;
+        case "stuffed":
+            priceCrust = 200;
+            break;
+        case "gluten":
+            priceCrust = 150;
+            break;
+        default:
+            location.reload();
+            alert("Please choose a crust");
+    };
+
+    if (pizzaSize == 'large') {
+        priceTopping = pizzaTopping.length * 150;
+    } else if (pizzaSize == 'medium') {
+        priceTopping = pizzaTopping.length * 100;
+    } else if (pizzaSize == 'small') {
+        priceTopping = pizzaTopping.length * 50;
+    }
+
+    var pizzaTotal = priceSize + priceCrust + priceTopping;
+    return pizzaTotal;
+}
 
 
 //user interface logic

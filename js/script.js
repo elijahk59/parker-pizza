833385d145affac992f333b0a70cddf608e8af16
elijahk59 <elijahk59@gmail.com>
2020-05-19 23:42:21 +0300

    added .ready function to js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//business interface logic
+
+//user interface logic
+$(document).ready(function() {
+    $("#orders").click(function() {
+        $(".plate2").show();
+        $(".plate1").hide();
+    })
+    $("#order").click(function() {
+        $(".plate2").show();
+        $(".plate1").hide();
+    })
+})
